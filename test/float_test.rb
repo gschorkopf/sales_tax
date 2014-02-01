decf60146cf2e27deb9cd9eca6ca810946e71015
Bryana Knight <brknig11@gmail.com>
2014-02-01 13:53:52 -0700

    add float test

---
+++ test/float_test.rb
@@ -0,0 +1,13 @@
+require 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/float.rb'
+
+class FloatTest < MiniTest::Unit::TestCase
+
+  def test_point_five
+    assert_equal 0.6, (0.5625).point_five
+    assert_equal 7.15, (7.125).point_five
+  end
+
+
+end
