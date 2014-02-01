abb7b8c805c34bf1d901b89975183ff2597c3618
Bryana Knight <brknig11@gmail.com>
2014-02-01 13:50:49 -0700

    updated test for string methods

--- lib/string.rb
+++ lib/string.rb
@@ -1,11 +1,19 @@
 class String
 
   def exempt?
-    include?("book") || include?("chocolate") || include?("pills")
+    exempt_items.any? { |item| self.include?(item) }
   end
 
   def import?
-    include?("import")
+    imported.any? { |word| self.include?(word) }
+  end
+
+  def exempt_items
+    ["book", "chocolate", "pills"]
+  end
+
+  def imported
+    ["import", "imported"]
   end
 
 end
---
+++ test/string_test.rb
@@ -0,0 +1,24 @@
+require 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/string.rb'
+
+class StringTest < MiniTest::Unit::TestCase
+
+  def test_exempt?
+    assert "1 book at 12.49".exempt?
+    assert "1 chocolate bar at 0.85".exempt?
+    assert "1 packet of headache pills at 9.75".exempt?
+    refute "1 music CD at 14.99".exempt?
+    refute "1 imported bottle of perfume at 47.50".exempt?
+  end
+
+  def test_import?
+    assert "1 imported bottle of perfume at 47.50".import?
+    assert " box of imported chocolates at 11.25".import?
+    refute "1 book at 12.49".import?
+    refute "1 packet of headache pills at 9.75".import?
+  end
+
+
+
+end
